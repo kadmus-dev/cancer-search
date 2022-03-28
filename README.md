3286a8ada5284fcfde07b78b858ff0debbd74577
Denikozub <denikozub@gmail.com>
2022-03-28 19:30:40 +0300

    change dataset

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 [Segmentation models](https://github.com/qubvel/segmentation_models.pytorch)  
 [Extra data](https://warwick.ac.uk/fac/cross_fac/tia/data/)  
+
+~~~
+python main.py Configs/cnfg.txt
+tensorboard --logdir runs
+~~~
--- segmentation/Dataset.py
+++ segmentation/Dataset.py
@@ -1,8 +1,12 @@
 import albumentations as A
 import albumentations.pytorch as Ap
 import cv2
+import numpy as np
 import pandas as pd
 import torch
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = 1000000000
 
 
 class ClassificationDataset(torch.utils.data.Dataset):
@@ -15,14 +19,17 @@ class ClassificationDataset(torch.utils.data.Dataset):
         if is_train:
             self.augmentations = A.Compose([
                 A.Resize(imsize, imsize),
-                # A.Flip(p=0.75),
-                A.Normalize(mean=(0., 0., 0.), std=(1., 1., 1.)),
+                A.Flip(p=0.8),
+                A.Rotate(limit=10, border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0), mask_value=(0, 0, 0)),
+                A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.2, p=0.5),
+                A.Sharpen(),
+                A.Normalize(mean=(0., 0., 0.), std=(1., 1., 1.), max_pixel_value=255.0),
                 Ap.ToTensorV2(),
             ])
         else:
             self.augmentations = A.Compose([
                 A.Resize(imsize, imsize),
-                A.Normalize(mean=(0., 0., 0.), std=(1., 1., 1.)),
+                A.Normalize(mean=(0., 0., 0.), std=(1., 1., 1.), max_pixel_value=255.0),
                 Ap.ToTensorV2(),
             ])
 
@@ -30,16 +37,13 @@ class ClassificationDataset(torch.utils.data.Dataset):
 
         img_path = self.data_path + self.data.iloc[index]["name"] + ".jpg"
         mask_path = self.data_path + self.data.iloc[index]["name"] + "_mask.jpg"
-        img = cv2.imread(img_path)
-        mask = cv2.imread(mask_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image = np.array(Image.open(img_path).convert('RGB'))
+        mask = np.array(Image.open(mask_path).convert('L'), dtype=np.float32)
+        mask = np.round(mask / 255).astype(np.float32)
 
-        aug_res = self.augmentations(image=img, mask=mask)
+        aug_res = self.augmentations(image=image, mask=mask)
         img = aug_res["image"]
-        mask = aug_res["mask"].float().mean(axis=2)
-
-        mask[mask < 128] = 0
-        mask[mask >= 128] = 1
+        mask = aug_res["mask"]
 
         return img, mask
 
